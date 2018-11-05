d90deb0ad141fb02ca460461f0abe7b3d02e4ec5
makemyA <henrot.paul@gmail.com>
2018-11-05 21:04:32 +0100

    eeee

--- src/Components/Vues/HomePage/Home.js
+++ src/Components/Vues/HomePage/Home.js
@@ -2,22 +2,14 @@
 import React, { Component } from 'react';
 import Header from '../../Global/Header';
 import CarouselHome from './Carousel';
-import {Grid, Row, Col} from 'react-bootstrap';
 export default class HomePage extends Component {
     render() {
         return(
             <div>
                 <Header />
-                <Grid>
-                    <Row>
-                        <Col componentClass= 'p'sm={6} lg={12} className='text'>
-                            "Hi, I'm Paul. Im a full stack Web Developer
-                        </Col>
-                        <Col sm={6} lg={12} className='text'>
-                        especially interested by JavaScript environment."
-                        </Col>
-                    </Row>
-                </Grid>
+                <div>
+                    <h1>test</h1>
+                </div>
                 <CarouselHome />    
             </div>
             
