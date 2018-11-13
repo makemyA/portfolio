25db524378c29d22b0febeeb7d22d9d0bc67b7c2
makemyA <henrot.paul@gmail.com>
2018-11-13 14:13:57 +0100

    Updates

---
+++ precache-manifest.df778c23fefdc83bbcbba4e71544c864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "218e33acc4861efa4aeaec392c6d481a",
+    "url": "/portfolio/static/media/LibreBaskerville-Regular.218e33ac.ttf"
+  },
+  {
+    "revision": "f89cba914f82dbc5382a",
+    "url": "/portfolio/static/js/runtime~main.f89cba91.js"
+  },
+  {
+    "revision": "bf3afe31bb78b617d180",
+    "url": "/portfolio/static/js/main.bf3afe31.chunk.js"
+  },
+  {
+    "revision": "0af7e20e71a382d3ca0e",
+    "url": "/portfolio/static/js/2.0af7e20e.chunk.js"
+  },
+  {
+    "revision": "bf3afe31bb78b617d180",
+    "url": "/portfolio/static/css/main.d31c6d85.chunk.css"
+  },
+  {
+    "revision": "81d372a522e92f9337934ab67cb92e14",
+    "url": "/portfolio/index.html"
+  }
+];
